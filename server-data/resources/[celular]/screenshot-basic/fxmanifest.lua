f93d9edbe0fc0d07b7e7d7d5fc607b7fbfe26d85
Kaio <kairodrigues736@gmail.com>
2022-09-04 18:55:47 -0300

    ok

---
+++ server-data/resources/[celular]/screenshot-basic/fxmanifest.lua
@@ -0,0 +1,14 @@
+shared_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+fx_version 'bodacious'
+game 'common'
+
+client_script 'dist/client.js'
+server_script 'dist/server.js'
+
+files {
+    'dist/ui.html'
+}
+
+ui_page 'dist/ui.html'
+              
